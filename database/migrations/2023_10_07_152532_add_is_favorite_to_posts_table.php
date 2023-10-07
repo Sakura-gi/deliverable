03aa43d2336248331fc73a11cae887da8627f132
Sakura-gi <kuragishi.ngo@gmail.com>
2023-10-08 00:42:46 +0900

    2023-1008-0039

--- database/migrations/2023_10_07_152532_add_is_favorite_to_posts_table.php
+++ database/migrations/2023_10_07_152532_add_is_favorite_to_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->boolean('is_favorite')->default(false); // デフォルト値を設定できます
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_favorite');
         });
     }
 };
